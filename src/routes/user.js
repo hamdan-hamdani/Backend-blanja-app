44428dc80c9bff686b246f68b003b9b188ff5928
Hamdan <hamdan230598@gmail.com>
2020-09-13 12:35:06 +0700

    edit endpoint di user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,9 +3,9 @@ const userController = require('../controllers/user')
 const router = express.Router()
 
 router
-  .post('/registercustomer', userController.registerCustomer)
-  .post('/logincustomer', userController.login)
+  .post('/register', userController.register)
+  .post('/login', userController.login)
   .post('/reset', userController.resetPassword)
-  .patch('/registercustomer/:email', userController.updatePassword)
+  .patch('/register/:email', userController.updatePassword)
 
 module.exports = router
